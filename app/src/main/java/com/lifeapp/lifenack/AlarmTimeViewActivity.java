1d58ea9b90d4d269d7360f0c7ec3c8b42b9736ca
toki666 <toki666@naver.com>
2021-05-12 00:09:10 +0900

    처음커밋

--- app/src/main/java/com/lifeapp/lifenack/AlarmTimeViewActivity.java
+++ app/src/main/java/com/lifeapp/lifenack/AlarmTimeViewActivity.java
@@ -1,56 +1,112 @@
 package com.lifeapp.lifenack;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 public class AlarmTimeViewActivity extends AppCompatActivity {
 
-    TextView alarmSetTime;
+    ImageView imageView;
+    String imgName = "osz.png";    // 이미지 이름
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_time_view);
+        imageView = findViewById(R.id.imageView);
 
-        alarmSetTime=findViewById(R.id.tv_alarm_set_day_time);
-
-        Intent intent=getIntent();
-        int setDay=intent.getIntExtra("setDay",0);
-        String day="";
-        switch (setDay){
-            case 0:
-                day="err";
-                break;
-            case 1:
-                day="일요일";
-                break;
-            case 2:
-                day="월요일";
-                break;
-            case 3:
-                day="화요일";
-                break;
-            case 4:
-                day="수요일";
-                break;
-            case 5:
-                day="목요일";
-                break;
-            case 6:
-                day="금요일";
-                break;
-            case 7:
-                day="토요일";
-                break;
-            default:
-                day="err";
-                break;
+        try {
+            String imgpath = getCacheDir() + "/" + imgName;   // 내부 저장소에 저장되어 있는 이미지 경로
+            Bitmap bm = BitmapFactory.decodeFile(imgpath);
+            imageView.setImageBitmap(bm);   // 내부 저장소에 저장된 이미지를 이미지뷰에 셋
+            Toast.makeText(getApplicationContext(), "파일 로드 성공", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "파일 로드 실패", Toast.LENGTH_SHORT).show();
         }
+    }
 
-        String setTime=day+"\n"+"알람 셋팅 시간"+intent.getStringExtra("setTime");
-        alarmSetTime.setText(setTime);
-    }// onCreate()..
-}// AlarmTimeViewActivity class..
+    public void bt1(View view) {    // 이미지 선택 누르면 실행됨 이미지 고를 갤러리 오픈
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 101);
+    }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) { // 갤러리
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            if (resultCode == RESULT_OK) {
+                Uri fileUri = data.getData();
+                ContentResolver resolver = getContentResolver();
+                try {
+                    InputStream instream = resolver.openInputStream(fileUri);
+                    Bitmap imgBitmap = BitmapFactory.decodeStream(instream);
+                    imageView.setImageBitmap(imgBitmap);    // 선택한 이미지 이미지뷰에 셋
+                    instream.close();   // 스트림 닫아주기
+                    saveBitmapToJpeg(imgBitmap);    // 내부 저장소에 저장
+                    Toast.makeText(getApplicationContext(), "파일 불러오기 성공", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "파일 불러오기 실패", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    public void saveBitmapToJpeg(Bitmap bitmap) {   // 선택한 이미지 내부 저장소에 저장
+        File tempFile = new File(getCacheDir(), imgName);    // 파일 경로와 이름 넣기
+        try {
+            tempFile.createNewFile();   // 자동으로 빈 파일을 생성하기
+            FileOutputStream out = new FileOutputStream(tempFile);  // 파일을 쓸 수 있는 스트림을 준비하기
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);   // compress 함수를 사용해 스트림에 비트맵을 저장하기
+            out.close();    // 스트림 닫아주기
+            Toast.makeText(getApplicationContext(), "파일 저장 성공", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "파일 저장 실패", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public Bitmap rotateBitmap(Bitmap bitmap, int rotate) {
+
+        Log.d("TEST", "ROTATE : " + rotate);
+        Matrix rotateMatrix = new Matrix();
+
+        if (rotate == 0)
+            rotateMatrix.postRotate(0);
+        else if (rotate == 1)
+            rotateMatrix.postRotate(45);
+        else if (rotate == 2)
+            rotateMatrix.postRotate(90);
+        else if (rotate == 3)
+            rotateMatrix.postRotate(135);
+        else if (rotate == 4)
+            rotateMatrix.postRotate(180);
+        else if (rotate == 5)
+            rotateMatrix.postRotate(225);
+        else if (rotate == 6)
+            rotateMatrix.postRotate(270);
+        else
+            rotateMatrix.postRotate(315);
+
+        Bitmap sideInversionImg = Bitmap.createBitmap(bitmap, 0, 0,
+                bitmap.getWidth(), bitmap.getHeight(), rotateMatrix, false);
+
+        return sideInversionImg;
+    }
+}
