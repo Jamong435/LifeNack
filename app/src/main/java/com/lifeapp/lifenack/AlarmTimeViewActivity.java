303b2d7d891e2ea3e2095d2b6c60678a73ea4fe8
younghwankim <sesang435@naver.com>
2021-07-02 00:23:34 +0900

    광고 커밋

--- app/src/main/java/com/lifeapp/lifenack/AlarmTimeViewActivity.java
+++ app/src/main/java/com/lifeapp/lifenack/AlarmTimeViewActivity.java
@@ -21,98 +21,20 @@ import java.io.InputStream;
 
 public class AlarmTimeViewActivity extends AppCompatActivity {
 
-    ImageView imageView;
-    String imgName = "osz.png";    // 이미지 이름
+    private Intent intent;
+    private final String packageNames = "kr.go.hrd.app";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_time_view);
-        imageView = findViewById(R.id.imageView);
 
-        try {
-            String imgpath = getCacheDir() + "/" + imgName;   // 내부 저장소에 저장되어 있는 이미지 경로
-            Bitmap bm = BitmapFactory.decodeFile(imgpath);
-            imageView.setImageBitmap(bm);   // 내부 저장소에 저장된 이미지를 이미지뷰에 셋
 
-        } catch (Exception e) {
-            Toast.makeText(getApplicationContext(), "실패하였습니다", Toast.LENGTH_SHORT).show();
-        }
+        intent = this.getPackageManager().getLaunchIntentForPackage(packageNames);
     }
 
-    public void bt1(View view) {    // 이미지 선택 누르면 실행됨 이미지 고를 갤러리 오픈
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, 101);
-    }
+    public void clickbtninstart(View view) {
+        AlarmTimeViewActivity.this.startActivity(intent);
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) { // 갤러리
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 101) {
-            if (resultCode == RESULT_OK) {
-                Uri fileUri = data.getData();
-                ContentResolver resolver = getContentResolver();
-                try {
-                    InputStream instream = resolver.openInputStream(fileUri);
-                    Bitmap imgBitmap = BitmapFactory.decodeStream(instream);
-                    imageView.setImageBitmap(imgBitmap);    // 선택한 이미지 이미지뷰에 셋
-                    instream.close();   // 스트림 닫아주기
-                    saveBitmapToJpeg(imgBitmap);    // 내부 저장소에 저장
-
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "파일 불러오기 실패", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
-
-    public void saveBitmapToJpeg(Bitmap bitmap) {   // 선택한 이미지 내부 저장소에 저장
-        File tempFile = new File(getCacheDir(), imgName);    // 파일 경로와 이름 넣기
-        try {
-            tempFile.createNewFile();   // 자동으로 빈 파일을 생성하기
-            FileOutputStream out = new FileOutputStream(tempFile);  // 파일을 쓸 수 있는 스트림을 준비하기
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);   // compress 함수를 사용해 스트림에 비트맵을 저장하기
-            out.close();    // 스트림 닫아주기
-
-        } catch (Exception e) {
-            Toast.makeText(getApplicationContext(), "파일 저장 실패", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public Bitmap rotateBitmap(Bitmap bitmap, int rotate) {
-
-        Log.d("TEST", "ROTATE : " + rotate);
-        Matrix rotateMatrix = new Matrix();
-
-        if (rotate == 0)
-            rotateMatrix.postRotate(0);
-        else if (rotate == 1)
-            rotateMatrix.postRotate(45);
-        else if (rotate == 2)
-            rotateMatrix.postRotate(90);
-        else if (rotate == 3)
-            rotateMatrix.postRotate(135);
-        else if (rotate == 4)
-            rotateMatrix.postRotate(180);
-        else if (rotate == 5)
-            rotateMatrix.postRotate(225);
-        else if (rotate == 6)
-            rotateMatrix.postRotate(270);
-        else
-            rotateMatrix.postRotate(315);
-
-        Bitmap sideInversionImg = Bitmap.createBitmap(bitmap, 0, 0,
-                bitmap.getWidth(), bitmap.getHeight(), rotateMatrix, false);
-
-        return sideInversionImg;
-    }
-
-    public void clickbtn(View view) {
-
-        Intent intent1 = new Intent(this,MainActivity.class);
-        startActivity(intent1);
-    }
+}
 }
